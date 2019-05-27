eecea66736fc46932502c437c62035a0c0b0209e
shahi121 <shahinevadia@gmail.com>
2019-05-27 16:01:48 +0600

    add a new method called display

--- Hello.java
+++ Hello.java
@@ -2,6 +2,12 @@ public class myHello{
 
     public static void main(String[] args){
             System.out.println("Hello World");
+            display("Shahi")
+    }
+
+
+    public void display(String name){
+            System.out.println(name);
     }
 
 }
